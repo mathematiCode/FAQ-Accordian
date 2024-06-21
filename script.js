934d34c104736bc05a3b1a6b0f7746f5d869745e
mathematiCode <160360849+mathematiCode@users.noreply.github.com>
2024-06-21 17:44:29 -0500

    Fixed icon button functionality

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function showHideAnswer(jsElement, toggleButton) {
-    var currentDisplay = window.getComputedStyle(jsElement).display;
-
-    // Toggle the display property based on its current value
-    if (currentDisplay === "none") {
-        jsElement.style.display = "inline";
-        toggleButton.setAttribute('data-state', 'expanded');
-    } else {
-        jsElement.style.display = "none";
-        toggleButton.setAttribute('data-state', 'collapsed');
-    }
-};
-
-
-
-
 const answer1 = document.getElementById("answer1");
 const answer2 = document.getElementById("answer2");
 const answer3 = document.getElementById("answer3");
@@ -31,33 +15,35 @@ const question4 = document.getElementById("q4");
 
 
 
-toggleButton1.addEventListener("click", function () {
+
+function showHideAnswer(jsElement, toggleButton) {
+    let currentDisplay = window.getComputedStyle(jsElement).display;
+
+    // Toggle the display property based on its current value
+    if (currentDisplay === "none") {
+        jsElement.style.display = "inline";
+        toggleButton.setAttribute('data-state', 'expanded');
+    } else {
+        jsElement.style.display = "none";
+        toggleButton.setAttribute('data-state', 'collapsed');
+    }
+};
+
+question1.addEventListener("click", function () {
     showHideAnswer(answer1, toggleButton1);
+    console.log("Question Pressed!");
 });
 
-q1.addEventListener("click", function () {
-    showHideAnswer(answer1, toggleButton1);
-});
-
-toggleButton2.addEventListener("click", function () {
+question2.addEventListener("click", function () {
     showHideAnswer(answer2, toggleButton2);
 });
-q2.addEventListener("click", function () {
-    showHideAnswer(answer2, toggleButton2);
-});
 
-toggleButton3.addEventListener("click", function () {
+
+question3.addEventListener("click", function () {
     showHideAnswer(answer3, toggleButton3);
 });
 
-q3.addEventListener("click", function () {
-    showHideAnswer(answer3, toggleButton3);
-});
 
-toggleButton4.addEventListener("click", function () {
-    showHideAnswer(answer4, toggleButton4);
-});
-
-q4.addEventListener("click", function () {
+question4.addEventListener("click", function () {
     showHideAnswer(answer4, toggleButton4);
 });
